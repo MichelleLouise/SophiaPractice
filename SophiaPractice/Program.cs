d69376dcb9e74c252b2d513f66083f5648aff4b0
michelle.oliver <michelle.oliver@abide-financial.com>
2017-07-14 12:38:08 +0100

    ABC-123 - Initial commit

---
+++ SophiaPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SophiaPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var helloWorld = "Hello World";
+            Console.WriteLine(helloWorld);
+            Console.ReadLine();
+        }
+    }
+}
